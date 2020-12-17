3cd5eddb6e3166e077a7a2f64444ffb29c654054
XHS <xu_watson@163.com>
2020-12-17 15:33:22 +0800

    modify

--- eval_pr_curve.py
+++ eval_pr_curve.py
@@ -66,15 +66,29 @@ def plot_pr_curve(config_file, result_file, metric="bbox"):
     A: area range, (all, small, medium, large), idx from 0 to 3
     M: max dets, (1, 10, 100), idx from 0 to 2
     '''
-    pr_array1 = precisions[0, :, 0, 0, 2] # iou@0.5
-    pr_array2 = precisions[2, :, 0, 0, 2] # iou@0.6
-    pr_array3 = precisions[5, :, 0, 0, 2] # iou@0.75
+    pr_array1 = precisions[0, :, 0, 0, 2] 
+    pr_array2 = precisions[1, :, 0, 0, 2] 
+    pr_array3 = precisions[2, :, 0, 0, 2] 
+    pr_array4 = precisions[3, :, 0, 0, 2] 
+    pr_array5 = precisions[4, :, 0, 0, 2] 
+    pr_array6 = precisions[5, :, 0, 0, 2] 
+    pr_array7 = precisions[6, :, 0, 0, 2] 
+    pr_array8 = precisions[7, :, 0, 0, 2] 
+    pr_array9 = precisions[8, :, 0, 0, 2] 
+    pr_array10 = precisions[9, :, 0, 0, 2] 
 
     x = np.arange(0.0, 1.01, 0.01)
     # plot PR curve
-    plt.plot(x, pr_array1, 'b-', label="iou=0.5")
-    plt.plot(x, pr_array2, 'c-', label="iou=0.6")
-    plt.plot(x, pr_array3, 'y-', label="iou=0.75")
+    plt.plot(x, pr_array1, label="iou=0.5")
+    plt.plot(x, pr_array2, label="iou=0.55")
+    plt.plot(x, pr_array3, label="iou=0.6")
+    plt.plot(x, pr_array4, label="iou=0.65")
+    plt.plot(x, pr_array5, label="iou=0.7")
+    plt.plot(x, pr_array6, label="iou=0.75")
+    plt.plot(x, pr_array7, label="iou=0.8")
+    plt.plot(x, pr_array8, label="iou=0.85")
+    plt.plot(x, pr_array9, label="iou=0.9")
+    plt.plot(x, pr_array10, label="iou=0.95")
 
     plt.xlabel("recall")
     plt.ylabel("precison")
@@ -85,7 +99,7 @@ def plot_pr_curve(config_file, result_file, metric="bbox"):
     plt.show()
 
 if __name__ == "__main__":
-    plot_pr_curve(config_file=CONFIG_FILE, result_file=RESULT_FILE, metric="bbox")
+    plot_pr_curve(config_file=CONFIG_FILE, result_file=RESULT_FILE, metric="segm")
 
     
 
